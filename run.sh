1e8b8b3cd55d9317574bf3ffaf68c5ba8873e43c
Ronnie Flathers <rflathers@gmail.com>
2019-01-22 16:44:16 -0600

    honor creds env var

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ mkdir -p /etc/nginx/certs
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/selfsigned.key -out /etc/nginx/certs/selfsigned.crt -subj /C=US/ST=IL/L=Chicago/O=Ropnop/OU=Testing/CN=nginxtty.ropnop.com
 
-# openssl dhparam -out /etc/nginx/certs/dhparams.pem 2048 > /dev/null
 
+if [ -z ${GOTTY_CREDENTIALS} ]; then
+    /usr/local/bin/gotty -a 127.0.0.1 -w --reconnect --ws-origin '.*' /bin/bash &
+else
+    /usr/local/bin/gotty -a 127.0.0.1 -w --reconnect -c "${GOTTY_CREDENTIALS}" --ws-origin '.*' /bin/bash &
+fi
 
-/usr/local/bin/gotty -a 127.0.0.1 -w --reconnect --ws-origin '.*' /bin/bash &
 nginx -g 'daemon off;'
